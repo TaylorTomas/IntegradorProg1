29637911500e202616c91fb9e0bca7df15500226
TaylorTomas <ttaylor@udesa.edu.ar>
2022-11-22 22:17:39 -0300

    commit18

--- JS/Resultado-busqueda.js
+++ JS/Resultado-busqueda.js
@@ -2,25 +2,21 @@
     let linkimagen = "https://image.tmdb.org/t/p/w500"
 
     
-    let sectionResultadosPelis = document.getElementsByClassName(`.resultados-peliculas`)
-    let contenedorTituloPelis = document.getElementsByClassName(`.contenedorPelis`)
-    let sectionResultadosSeries = document.getElementsByClassName(`.resultados-series`)
-    let contenedorTituloSeries = document.getElementsByClassName(`.contenedorSeries`)
+    let sectionResultadosPelis = document.querySelector('.resultados-peliculas')
+    let contenedorTituloPelis = document.querySelector(`.contenedorPelis`)
+    let sectionResultadosSeries = document.querySelector(`.resultados-series`)
+    let contenedorTituloSeries = document.querySelector(`.contenedorSeries`)
 
     let queryString = location.search
     let queryStringObj = new URLSearchParams (queryString)
-    let queryStringObjBarraBusqueda = queryStringObj.get(`barrabusqueda`)
-    let queryStringObjTipoPeliculas = queryStringObj.get(`tipoPeliculas`)
-    let queryStringObjTipoSeries = queryStringObj.get(`tipoSeries`)
+    let queryStringObjBarraBusqueda = queryStringObj.get(`search`)
     
     console.log(queryString)
 
-
-if (queryStringObjTipoPeliculas == `peliculas`) {
+    
         
-
-        contenedorTituloPelis.innerHTML += 
-        `<h2>PELICULAS</h2>`
+    contenedorTituloPelis.innerHTML += 
+    `<h2>PELICULAS</h2>`
 
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=b76faeee5fc3002a166c7f5c929c2c33&language=en-US&query=${queryStringObjBarraBusqueda}&page=1&include_adult=false`)
         .then(function(response){
@@ -29,20 +25,16 @@ if (queryStringObjTipoPeliculas == `peliculas`) {
         .then(function(data){
             console.log(data);
 
-            for (let index = 0; index < data.results.length; index++) {
+            for (let index = 0; index < 10; index++) {
                 const element = data.results[index];
 
                 sectionResultadosPelis.innerHTML += 
-         `
-                
-                <a href="Descripcion-pelicula.html?id=${element.id}&tipo=pelicula">
-                <img class="Img"src="${linkimagen}${element.poster_path}" alt="">
-                <h2>${element.title}</h2>
-                </a>
+         
+                `<a href="Descripcion-pelicula.html?id=${element.id}&tipo=pelicula">
+                <img class="Img" src="${linkimagen}${element.poster_path}" alt="">
+                <h2 class="titulopeli">${element.title}</h2>
+                </a>`
 
-         `
-         
-         
             }
 
         })
@@ -50,9 +42,6 @@ if (queryStringObjTipoPeliculas == `peliculas`) {
         console.log(error);
     })
 
-    }
-
-if (queryStringObjTipoSeries == `series` ) {
        
 
        contenedorTituloSeries.innerHTML += `
@@ -66,17 +55,15 @@ if (queryStringObjTipoSeries == `series` ) {
         .then(function(data){
             console.log(data);
 
-            for (let index = 0; index < data.results.length; index++) {
+            for (let index = 0; index < 10; index++) {
                  const element = data.results[index];
                  
-                 sectionResultadosSeriesI.innerHTML += 
+                 sectionResultadosSeries.innerHTML += 
          
-                `<article class="article-peli">
-                <a href="detalle.html?id=${element.id}&tipo=serie">
-                <img src="${linkimagen}${element.poster_path}" alt="">
-                <h2>${element.name}</h2>
-                </a>
-                </article>`
+                `<a href="Descripcion-serie.html?id=${element.id}&tipo=pelicula">
+                <img class="Img" src="${linkimagen}${element.poster_path}" alt="">
+                <h2 class="titulopeli">${element.name}</h2>
+                </a>`
          
             }
 
@@ -84,11 +71,4 @@ if (queryStringObjTipoSeries == `series` ) {
         .catch(function(error){
             console.log(error);
         })
-
-    }
-
-
-
-
-
 
